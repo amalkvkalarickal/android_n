59182215d08a6c9e78bd6932af8c7528739c82ed
Amal <amalkvkalarikkal@gmail.com>
2025-04-29 14:42:00 +0530

    Fixed Gunicorn app setup

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# Add the project root directory to Python path
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.append(str(BASE_DIR))
+
+# Set the Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_name.settings')
+
+# Import the Django WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
-application = get_wsgi_application()
+app = get_wsgi_application()  # This is the 'app' that Gunicorn is looking for
