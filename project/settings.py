f356b3a48958f42dc2c20f4627ce73f706207002
Amal <amalkvkalarikkal@gmail.com>
2025-04-29 14:29:44 +0530

    Fixed Procfile for app and project naming

--- project/settings.py
+++ project/settings.py
@@ -128,9 +128,3 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
-application = get_wsgi_application()
